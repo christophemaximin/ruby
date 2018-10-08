cc0fb202c4991fa29abe4368e63ee34f9f87a1bb
Christophe Maximin <christophe.maximin@gmail.com>
2018-10-08 22:17:30 +0100

    Add Resolv.current_resolver

--- spec/ruby/library/resolv/get_addresses_spec.rb
+++ spec/ruby/library/resolv/get_addresses_spec.rb
@@ -12,3 +12,26 @@ describe "Resolv#getaddresses" do
     end
   end
 end
+
+describe "Resolv.getaddresses" do
+  it "calls DefaultResolver#getaddresses" do
+    Resolv::DefaultResolver.should_receive(:getaddresses).with("localhost")
+    Resolv.getaddresses("localhost")
+  end
+
+  ruby_version_is "2.6" do
+    context "with a custom resolver" do
+      after do
+        Resolv.current_resolver = nil
+      end
+
+      it "calls #getaddresses on the custom resolver" do
+        resolver = Resolv.new([])
+        resolver.should_receive(:getaddresses).with("localhost")
+
+        Resolv.current_resolver = resolver
+        Resolv.getaddresses("localhost")
+      end
+    end
+  end
+end
--- spec/ruby/library/resolv/get_names_spec.rb
+++ spec/ruby/library/resolv/get_names_spec.rb
@@ -12,3 +12,26 @@ describe "Resolv#getnames" do
     end
   end
 end
+
+describe "Resolv.getnames" do
+  it "calls DefaultResolver#getnames" do
+    Resolv::DefaultResolver.should_receive(:getnames).with("127.0.0.1")
+    Resolv.getnames("127.0.0.1")
+  end
+
+  ruby_version_is "2.6" do
+    context "with a custom resolver" do
+      after do
+        Resolv.current_resolver = nil
+      end
+
+      it "calls #getnames on the custom resolver" do
+        resolver = Resolv.new([])
+        resolver.should_receive(:getnames).with("127.0.0.1")
+
+        Resolv.current_resolver = resolver
+        Resolv.getnames("127.0.0.1")
+      end
+    end
+  end
+end
